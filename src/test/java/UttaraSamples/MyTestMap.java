c72f4cde2aa018ca79077252faa85a2fe5b7554a
KiranAmberkar1977 <kiranamberkar@gmail.com>
2020-10-28 22:58:58 +0530

    Third commit

--- src/test/java/UttaraSamples/MyTestMap.java
+++ src/test/java/UttaraSamples/MyTestMap.java
@@ -19,6 +19,7 @@ public class MyTestMap {
 		System.out.println("The Map contents are:" + m);
 		
 		for(Object o : m.keySet()) {
+			// Modified from int to Integer.
 			Integer myKey = (Integer)o;
 			System.out.println("Key is: " + myKey + "and value is:" + m.get(myKey) );
 		}
